b8bee73dc737c3f43c3d6be6e54d4b4f5e3e67e6
nicolasjadoul <jadoul.nico@yahoo.fr>
2023-11-22 22:12:18 +0100

    Initial Commit

---
+++ DotscreenTest/DotscreenTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotscreenTestApp.swift
+//  DotscreenTest
+//
+//  Created by Jadoul Nicolas on 22/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DotscreenTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
